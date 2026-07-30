3c292d8cb56f6b7acc0fc679a21f4c646878f28d
Jhay-Ru <jason0172back@gmail.com>
2026-07-30 12:17:55 +1000

    tone js setup
    Added tone library and text button

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-document.body.style.backgroundColor = "red";
+//document.body.style.backgroundColor = "red";
+const testButton = document.getElementById("test-button");
+
+//new instrument and connect to speakers
+const synth = new Tone.Synth().todestination();
+
+testButton.addEventListener("click", playTest);
+function playTest() {
+  synth.triggerAttackRelease("C4", "8n"); //play note
+}
