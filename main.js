a0c919af23b2dce6641e7cf6fae151281acf5ede
Jhay-Ru <jason0172back@gmail.com>
2026-08-13 10:55:10 +1000

    Added intro modal and audio init
    added a intro dialog and linked that up to the audio modal

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+//when browser loads > load dialog
+
+const introDialog = document.getElementById("Intro-dialog"); //get dialog element
+const introButtonClose = document.getElementById("Intro-button-close"); //get close button element
+
+//console.log(introDialog); //tester debugger
+introDialog.showModal(); //show dialog
+
+introButtonClose.addEventListener("click", function closeIntroDialog() {
+  //you can put a function directly into the event listener
+  introDialog.close(); //close dialog
+});
+
+introButtonClose.addEventListener("close", toneInit); //when user click close, load audio
+//closeIntroDialog.addEventListener("click", ()=> {IntroDialog.close();}); //you can also use an arrow function
+
 //document.body.style.backgroundColor = "red";
+//--- sound player from button ---
+
+//// audio
 const testButton = document.getElementById("test-button");
 
 //new instrument and connect to speakers
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth();
+
+function toneInit() {
+  synth.connect(Tone.Destination); //connect to speakers
+}
 
 testButton.addEventListener("click", playTest);
 function playTest() {
