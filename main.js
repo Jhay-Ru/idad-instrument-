4feda594da541f8c2af72c1eb7e7ad0f8ea8cdc0
Jhay-Ru <jason0172back@gmail.com>
2026-08-20 11:54:13 +1000

    music handler rewrite
    Made data notes a bit more dynamic and more flexible

--- main.js
+++ main.js
@@ -3,8 +3,16 @@
 const introDialog = document.getElementById("Intro-dialog"); //get dialog element
 const introButtonClose = document.getElementById("Intro-button-close"); //get close button element
 
+const key = document.getElementById("key-test");
 //console.log(introDialog); //tester debugger
 introDialog.showModal(); //show dialog
+
+/// is the mouse btn being held?      ///test key
+let mouseButtonDown = false;
+
+key.addEventListener("click", function () {
+  playTest();
+});
 
 introButtonClose.addEventListener("click", function closeIntroDialog() {
   //you can put a function directly into the event listener
@@ -20,13 +28,33 @@ introDialog.addEventListener("close", toneInit); //when user click close, load a
 const testButton = document.getElementById("test-button");
 
 //new instrument and connect to speakers
-const synth = new Tone.Synth();
+const synth = new Tone.PolySynth(); //polysynth to play multiple notes at once
 
 function toneInit() {
   synth.connect(Tone.Destination); //connect to speakers
 }
 
-testButton.addEventListener("click", playTest);
-function playTest() {
-  synth.triggerAttackRelease("C4", "8n"); //play note
+// testButton.addEventListener("click", function(e){
+//   console.log(e)
+//   playTest("C4");
+// });
+// function playTest(note) {
+//   synth.triggerAttackRelease(note, "8n"); //play note
+// }
+// key.addEventListener("click", function () {
+//   playTest("C4");
+// });
+
+function playNote(e) {
+  // find the element that the event ran on
+  let keyPressed = e.target;
+  console.log(keyPressed);
+  // find the data-note attribute of that element
+  let note = keyPressed.dataset.note;
+  console.log(note);
+  synth.triggerAttackRelease(note, "8n"); //play note
 }
+testButton.addEventListener("mousedown", playNote);
+key.addEventListener("mousedown", playNote);
+
+testButton.addEventListener("mousedown", playnote);
