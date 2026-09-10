672844d6f5e1dc3fe452386c7037bffd4b47e4dd
Jhay-Ru <jason0172back@gmail.com>
2026-09-10 11:44:52 +1000

    Data/sounds and more
    Added a data tag for each button, added audio plus moved notes A B from each octave at the end so notes would be in order

--- note-grid/main.js
+++ note-grid/main.js
@@ -1,5 +1,21 @@
-const synth = new Tone.PolySynth();
+// const synth = new Tone.PolySynth();
 
-function toneInit() {
-  synth.connect(Tone.Destination); //connect to speakers
-}
+// function toneInit() {
+//   synth.connect(Tone.Destination); //connect to speakers
+// }
+
+const synth = new Tone.Synth().toDestination();
+const keys = document.querySelectorAll(".key"); //selects all keys
+
+keys.forEach((key) => {
+  key.addEventListener("click", async () => {
+    await Tone.start();
+
+    // Pull the exact note string from data-note
+    const note = key.dataset.note;
+
+    if (note) {
+      synth.triggerAttackRelease(note, "8n"); //
+    }
+  });
+});
